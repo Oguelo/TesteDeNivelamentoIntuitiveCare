a4c64e76d3e02670f9b397755d59a2b64af3b5ff
Alexjr <88115079+Oguelo@users.noreply.github.com>
2025-03-29 23:20:27 -0300

    .
    .

--- Parte_tres_e_quatro/main.py
+++ Parte_tres_e_quatro/main.py
@@ -4,10 +4,23 @@ from src.banco_de_dados import criar_db, executar_arquivo_sql, obter_top_operado
 from src.importador import converter_utf8_sem_salvar, inserir_dados_formatados
 
 
+criar_db()
+executar_arquivo_sql("financeiro.sql")  
+
+
+if os.path.exists(CSV_FILE_CADASTRAL):
+    df_cadastral = converter_utf8_sem_salvar(CSV_FILE_CADASTRAL)
+    inserir_dados_formatados(df_cadastral, "registros_ans", DB_HOST, DB_USUARIO, DB_SENHA, DB_NOME)
+
+for file in CSV_FILES_CONTAS:
+    if os.path.exists(file):
+        df_contas = converter_utf8_sem_salvar(file)
+        inserir_dados_formatados(df_contas, "contas", DB_HOST, DB_USUARIO, DB_SENHA, DB_NOME)
+
+
 print("=== Top 10 Operadoras - Último Trimestre ===")
 df_trimestre = obter_top_operadoras(periodo_meses=3)
 print(df_trimestre.to_string(index=False))
-
 
 print("\n=== Top 10 Operadoras - Último Ano ===")
 df_ano = obter_top_operadoras(periodo_meses=12)
--- Parte_tres_e_quatro/src/banco_de_dados.py
+++ Parte_tres_e_quatro/src/banco_de_dados.py
@@ -5,44 +5,43 @@ from sqlalchemy import create_engine
 from settings import DB_HOST, DB_NOME, DB_SENHA, DB_USUARIO
 
 def conectar_mysql():
-    """Conecta ao MySQL com SQLAlchemy"""
+    
     engine = create_engine(f"mysql+pymysql://{DB_USUARIO}:{DB_SENHA}@{DB_HOST}/{DB_NOME}")
-    return engine.connect()  # Retorna a conexão com o banco de dados
+    return engine.connect() 
 
 def criar_db():
-    """Cria o banco de dados se não existir"""
+   
     conn = pymysql.connect(host=DB_HOST, user=DB_USUARIO, password=DB_SENHA, charset='utf8mb4')
     cursor = conn.cursor()
     cursor.execute(f"CREATE DATABASE IF NOT EXISTS {DB_NOME} CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci")
     conn.commit()
     conn.close()
 
-from sqlalchemy import text
 
 def executar_arquivo_sql(sql_file):
    
-    conn = conectar_mysql()  # Conecta usando SQLAlchemy
+    conn = conectar_mysql() 
 
     with open(sql_file, 'r', encoding='utf-8') as file:
         sql_commands = file.read()
 
     for command in sql_commands.split(';'):
         command = command.strip()
-        if command:  # Verifica se o comando não está vazio
-            conn.execute(text(command))  # Usa text() para transformar a string em uma consulta executável
+        if command: 
+            conn.execute(text(command)) 
     
-    conn.close()  # Fecha a conexão após a execução
+    conn.close()  
 
 
 
 def obter_top_operadoras(periodo_meses=12, limite=10):
-    conn = conectar_mysql()  # Função para conectar ao banco de dados MySQL
+    conn = conectar_mysql() 
     
-    # Definindo as datas de início e fim com base no período de 2023 a 2024
-    data_inicio = '2023-01-01'  # Início do ano de 2023
-    data_fim = '2024-12-31'     # Fim do ano de 2024
     
-    # A consulta com placeholders para a descrição única e as datas
+    data_inicio = '2023-01-01'  
+    data_fim = '2024-12-31'     
+    
+  
     query = """
     SELECT 
         r.Razao_Social AS Operadora,
@@ -61,11 +60,11 @@ def obter_top_operadoras(periodo_meses=12, limite=10):
     LIMIT %s
     """
 
-    # Parâmetro com a descrição única
+    
     params = ('%EVENTOS/ SINISTROS CONHECIDOS%', data_inicio, data_fim, limite)
 
     try:
-        # Usando o read_sql com parâmetros passados corretamente
+        
         df = pd.read_sql(query, conn, params=params)
         return df
     finally:
